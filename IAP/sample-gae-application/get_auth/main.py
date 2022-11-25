fec78689aba293a618edba1c9aef1014146135eb
Vasco Silva <vasco.antunes.silva@gmail.com>
2022-11-25 12:00:59 +0000

    add: first commit

---
+++ IAP/sample-gae-application/get_auth/main.py
@@ -0,0 +1,45 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python38_app]
+# [START gae_python3_app]
+from flask import Flask
+from flask import request
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return user email and internal Google ID"""
+    
+    if 'X-Goog-Authenticated-User-Email' in request.headers:
+        email = request.headers['X-Goog-Authenticated-User-Email']
+        userID = request.headers['X-Goog-Authenticated-User-Id']
+        return 'Hello ' + email + '. Your internal Google user id is: ' + userID
+    
+    
+    return 'Unable to get user authentication - You are calling this service in unauthenticated mode'
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python3_app]
+# [END gae_python38_app]
